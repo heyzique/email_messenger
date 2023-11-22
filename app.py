c9a1d8cf26170f09f25330fe1e65689137d8d094
heyzique <144900297+heyzique@users.noreply.github.com>
2023-11-22 12:01:24 +0800

    Creating a database and setting data base specs

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
+# --- Libraries ---
 from flask import Flask, render_template, request, flash
 from datetime import datetime
 
+# Library to connect to database using flask
+from flask_sqlalchemy import SQLAlchemy
+
+# Instances
 app = Flask(__name__)
 
+# Data Base Config
+app.config["SECRET_KEY"] = "sendmessage123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 
-# Running the application + linking to html file
+db = SQLAlchemy(app)
+
+
+# Data Base Model with field specifications
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    purpose = db.Column(db.String(80))
+    messages = db.Column(db.String(500))
+
+
+# --- Running the application + linking to html file ---
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
@@ -14,9 +36,14 @@ def index():
         date = request.form["date"]
         purpose = request.form["purpose"]
         messages = request.form["messages"]
+        print(request.method)
         print(first_name, last_name, email, date, purpose, messages)
     return render_template("index.html")
 
 
+# Creating new database (if no existing database matching the set URI
+# & run app on local terminal
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    with app.app_context():
+        db.create_all()
+        app.run(debug=True, port=5001)
