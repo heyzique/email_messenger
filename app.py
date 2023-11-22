fce4bcf44434a2edfa8d525a29f4b08e302f6993
heyzique <144900297+heyzique@users.noreply.github.com>
2023-11-22 11:39:55 +0800

    Getting user data from the form html

--- app.py
+++ app.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 
 
 # Running the application + linking to html file
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        purpose = request.form["purpose"]
+        messages = request.form["messages"]
+        print(first_name, last_name, email, date, purpose, messages)
     return render_template("index.html")
 
 
