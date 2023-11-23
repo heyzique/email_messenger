e51945212492d02ff1e7ad50b786b1da29dec9cc
heyzique <144900297+heyzique@users.noreply.github.com>
2023-11-23 15:17:23 +0800

    Completed code

--- app.py
+++ app.py
@@ -18,26 +18,26 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = "haziqdanial.hyde@gmail.com"
-app.config["MAIL_PASSWORD"] = "gbbh vluu xwqu wbvm"
+app.config["MAIL_USERNAME"] = "#username"
+app.config["MAIL_PASSWORD"] = "#password"
 
-#db = SQLAlchemy(app)
+db = SQLAlchemy(app)
 mail = Mail(app)
 
 
 # Data Base Model with field specifications
-#class Form(db.Model):
-#    id = db.Column(db.Integer, primary_key=True)
-#    first_name = db.Column(db.String(80))
-#    last_name = db.Column(db.String(80))
-#    email = db.Column(db.String(80))
-#    date = db.Column(db.Date)
-#    purpose = db.Column(db.String(80))
-#    messages = db.Column(db.String(500))
+class Form(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    purpose = db.Column(db.String(80))
+    messages = db.Column(db.String(500))
 
 
 # --- Running the application + linking to html file ---
-@app.route("/", methods=["POST"])
+@app.route("/", methods=['GET', 'POST'])
 def index():
     if request.method == "POST":
         first_name = request.form["first_name"]
@@ -51,22 +51,25 @@ def index():
         print(first_name, last_name, email, date, purpose, messages)
 
         # Creating variables to store user input
-        #form = Form(first_name=first_name, last_name=last_name,
-        #            email=email, date=date_obj, purpose=purpose, messages=messages)
+        form = Form(first_name=first_name, last_name=last_name,
+                    email=email, date=date_obj, purpose=purpose, messages=messages)
 
         # Updating variables to the db session and committing to db
-        #db.session.add(form)
-        #db.session.commit()
+        db.session.add(form)
+        db.session.commit()
 
+        # User/Client Message
         message_body = f"<NO REPLY>\n\nDate: {date}\nThank you for taking the time to write me a message, {first_name}!\n\n" \
                        f"Here are the details submitted. \nName: {first_name} {last_name}\nPurpose: {purpose} " \
                        "\n\nI will get back to you soon. Hope you have a blessed day!\n\n " \
                        "<NO REPLY: Please do not reply to this email!>"
 
+        # Admin Message
         ownmessage_body = "You just recieved a message from EMAIL MESSENGER!\n\n" \
                           f"Sender Name: {first_name} {last_name}\nSender Email: {email}\nPurpose: {purpose}\n" \
                           f"Message: {messages}"
 
+        # -- Email Format --
         clientmessage = Message(subject="Your Message Submitted Successfully!",
                           sender=app.config["MAIL_USERNAME"],
                           recipients=[email],
@@ -90,5 +93,5 @@ def index():
 # & run app on local terminal
 if __name__ == "__main__":
     with app.app_context():
-        #db.create_all()
+        db.create_all()
         app.run(debug=True, port=5001)
